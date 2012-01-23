42eac15260ba6aeeeb6fe8136575db6d7108fab6
Sam Ulu <sam.e.ulu@gmail.com>
2012-01-23 12:13:57 -0500

    updated gem rspec rails for beta18

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,16 +1,20 @@
 require 'spec_helper'
 
 describe PagesController do
-render_views	
+render_views
+
+before(:each) do
+	@base_title = "Ruby on Rails Tutorial Sample App"
+end
 
   describe "GET 'home'" do
-    	it "should be successful" do
+       	it "should be successful" do
       	get 'home'
       	response.should be_success
     	end
 	it "should have the right title" do
 	get 'home'
-	response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample App | Home")
+	response.should have_selector("title",:content => "#{@base_title} | Home")
   	end
 	it "should have a non-blank body" do
 	get 'home'
@@ -26,7 +30,7 @@ render_views
 
 	it "should have the right title" do
 	get 'contact'
-	response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample App | Contact")
+	response.should have_selector("title",:content => "#{@base_title} | Contact")
 	end
   end
 
@@ -38,7 +42,7 @@ render_views
 
 	it "should have the right title" do
 	get 'about'
-	response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample App | About")
+	response.should have_selector("title",:content => "#{@base_title} | About")
  	end
   end
 
